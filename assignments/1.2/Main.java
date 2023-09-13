71b0877110a5a565cd78bba10bfc325c38265299
Linus Liang <lliang5680@bths.edu>
2023-09-13 08:54:18 -0400

    Assignment 1.2

---
+++ assignments/1.2/Main.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        final int teams = 2; // game only allows for 2 teams so it's final
+        double totalPoints = 127.5;
+        boolean isTie = true;
+
+        System.out.println("There are " + teams + " teams, and in total the teams earned " + totalPoints + " points. And the statement the game was a tie is " + isTie + ".");
+    }
+}
